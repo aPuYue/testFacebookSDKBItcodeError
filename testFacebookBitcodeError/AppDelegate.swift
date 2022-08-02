74a077b11d432b04976138f91b226b919d469b75
aPuYue <pu.yue@access-company.com>
2022-08-02 17:25:19 +0900

    go on

--- testFacebookBitcodeError/AppDelegate.swift
+++ testFacebookBitcodeError/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AppLovinSDK
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        ALSdk.shared()!.mediationProvider = "max"
+        ALSdk.shared()!.initializeSdk { (configuration: ALSdkConfiguration) in
+            // AppLovin SDK is initialized, start loading ads
+        }
         return true
     }
 
