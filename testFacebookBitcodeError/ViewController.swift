9f006529a69929e815a6b3ce9b6cba5a51375d8d
aPuYue <pu.yue@access-company.com>
2022-08-02 16:52:36 +0900

    1

---
+++ testFacebookBitcodeError/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testFacebookBitcodeError
+//
+//  Created by Pu Yue - PU YUE on 2022/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
